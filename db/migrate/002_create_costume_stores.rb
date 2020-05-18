0ca7a37f61d16458ea6f995d452db27406cdfce7
Megan Comeau <mlcomeau99@gmail.com>
2020-05-18 22:28:44 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,13 @@
 # Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[5.2]
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.float :price
+      t.string :image_url
+      t.string :size
+      t.datetime :created_at
+      t.datetime :updated_at
+    end
+  end
+end
